0becca93ad43a3427da6af56057080f8892a3d82
eles <ivaneleskin2019@gmail.com>
2021-09-28 18:13:48 +0300

    add edit profile

--- geekshop/users/forms.py
+++ geekshop/users/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, User
 
 from django import forms
 
-from users.models import User
+from users.models import User, UserProfile
 
 
 class UserProfileForm(UserChangeForm):
@@ -94,3 +94,16 @@ class UserRegisterForm(UserCreationForm):
         user.save()
         return user
 
+class UserProfileEditForm(forms.ModelForm):
+
+    class Meta:
+        model = UserProfile
+        fields = ('tagline', 'gender','about_me')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != 'gender':
+                field.widget.attrs['class'] = 'form-control py-4'
+            else:
+                field.widget.attrs['class'] = 'form-control'
