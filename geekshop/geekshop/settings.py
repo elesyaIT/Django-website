54f0d9eff9768f4897715bf07dd01bcea0d3ebd1
eles <ivaneleskin2019@gmail.com>
2021-10-18 14:59:56 +0300

    add cashe

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
