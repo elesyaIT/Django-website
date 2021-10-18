694f1a8d5a1e5b9116f865702d0cc6ad57eab4e4
eles <ivaneleskin2019@gmail.com>
2021-10-18 12:54:37 +0300

    add activ

--- geekshop/baskets/models.py
+++ geekshop/baskets/models.py
@@ -1,11 +1,25 @@
+import statistics
+
 from django.db import models
+from django.utils.functional import cached_property
 
 from users.models import User
 from products.models import Product
 
 
+# class BasketQuerySet(models.QuerySet):
+#
+#     def delete(self,*args,**kwargs):
+#         for item in self:
+#             item.product.quantity += item.quantity
+#             item.product.save()
+#         super(BasketQuerySet, self).delete(*args,**kwargs)
+
+
+
 class Basket(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # objects = BasketQuerySet.as_manager()
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
 
@@ -18,10 +32,36 @@ class Basket(models.Model):
     def sum(self):
         return self.quantity * self.product.price
 
-    def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
-        return sum(basket.quantity for basket in baskets)
+    # def sum(self):
+    #     return self.quantity * self.product.price
+
 
     def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.sum() for basket in baskets)
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
+    def total_quantity(self):
+        baskets = self.get_items_cached
+        return sum(basket.quantity for basket in baskets)
+
+
+    # def delete(self,*args,**kwargs):
+    #     self.product.quantity +=self.quantity
+    #     self.save()
+    #     super(Basket, self).delete(*args,**kwargs)
+    #
+    # def save(self,*args,**kwargs):
+    #     if self.pk:
+    #         self.product.quantity -= self.quantity - self.get_item(int(self.pk))
+    #     else:
+    #         self.product.quantity -= self.quantity
+    #     self.product.save()
+    #     super(Basket, self).save(*args,**kwargs)
+
+    @staticmethod
+    def get_item(pk):
+        return Basket.objects.get(pk=pk).quantity
