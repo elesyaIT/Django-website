17e522a76c1dbff86513bad661c656ce26583051
eles <ivaneleskin2019@gmail.com>
2021-10-18 14:53:58 +0300

    add cashe

--- geekshop/products/urls.py
+++ geekshop/products/urls.py
@@ -1,46 +1,46 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.cache import cache_page
-
-app_name = 'products'
-
-from products.views import products,ProductDetail
-
-urlpatterns = [
-    # path('', cache_page(3600)(products), name='index'),
-    path('', products, name='index'),
-    path('category/<int:id>', products, name='category'),
-    path('page/<int:page>', products, name='page'),
-    path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from django.contrib import admin
-# from django.urls import path
 #
 # app_name = 'products'
 #
-# from products.views import ProductListView, ProductDetail #products
+# from products.views import products,ProductDetail
 #
 # urlpatterns = [
-#     # path('', products, name='index'),
-#     path('', ProductListView.as_view(), name='index'),
-#     path('category/<int:pk>', ProductListView, name='category'),
-#     path('page/<int:page>', ProductListView, name='page'),
+#     # path('', cache_page(3600)(products), name='index'),
+#     path('', products, name='index'),
+#     path('category/<int:id>', products, name='category'),
+#     path('page/<int:page>', products, name='page'),
 #     path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
 # ]
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from django.contrib import admin
+from django.urls import path
+
+app_name = 'products'
+
+from products.views import ProductListView, ProductDetail #products
+
+urlpatterns = [
+    # path('', products, name='index'),
+    path('', ProductListView.as_view(), name='index'),
+    path('category/<int:pk>', ProductListView, name='category'),
+    path('page/<int:page>', ProductListView, name='page'),
+    path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
+]
 
--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,128 +1,24 @@
 
-
-
-
-
-
-from django.conf import settings
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-from django.core.cache import cache
-
-import os
-import json
-
-from django.views.decorators.cache import cache_page, never_cache
-from django.views.generic import DetailView
-
-from products.models import Product, ProductsCategory
-
-MODULE_DIR = os.path.dirname(__file__)
-
-
-# Create your views here.
-# Контролер - функция
-
-def get_links_category():
-    if settings.LOW_CACHE:
-        key = 'links_category'
-        links_category = cache.get(key)
-
-        if links_category is None:
-            links_category = ProductsCategory.objects.filter(is_active=True)
-            cache.set(key, links_category)
-        return links_category
-    else:
-        return ProductsCategory.objects.filter(is_active=True)
-
-def get_product(pk):
-    if settings.LOW_CACHE:
-        key = f'product{pk}'
-        product = cache.get(key)
-
-        if product is None:
-            product = get_object_or_404(Product,pk=pk)
-            cache.set(key, product)
-        return product
-    else:
-        return get_object_or_404(Product,pk=pk)
-
-
-def get_links_product():
-    if settings.LOW_CACHE:
-        key = 'links_product'
-        links_product = cache.get(key)
-
-        if links_product is None:
-            links_product = Product.objects.filter(is_active=True).select_related()
-            cache.set(key, links_product)
-        return links_product
-    else:
-        return Product.objects.filter(is_active=True).select_related()
-
-
-def index(request):
-    context = {'title': 'GeekShop'}
-    return render(request, 'products/index.html', context)
-@cache_page(3600)
-def products(request, id=None, page=1):
-    products = Product.objects.filter(category_id=id).select_related(
-        'category') if id != None else Product.objects.all().select_related('category')
-    # products = get_links_product()
-    paginator = Paginator(products, per_page=3)
-    try:
-        products_paginator = paginator.page(page)
-    except PageNotAnInteger:
-        products_paginator = paginator.page(1)
-    except EmptyPage:
-        products_paginator = paginator.page(paginator.num_pages)
-
-    context = {'title': 'Каталог',
-               'category': ProductsCategory.objects.filter(is_active=True),
-               }
-    context['products'] = products_paginator
-    return render(request, 'products/products.html', context)
-
-
-class ProductDetail(DetailView):
-    """
-    Контроллер вывода информации о продукте
-    """
-    model = Product
-    template_name = 'products/product_detail.html'
-    context_object_name = 'product'
-
-
-    def get_context_data(self, category_id=None, *args, **kwargs):
-        """Добавляем список категорий для вывода сайдбара с категориями на странице каталога"""
-        context = super().get_context_data()
-
-        context['product'] = get_product(self.kwargs.get('pk'))
-        context['categories'] = ProductsCategory.objects.all()
-        return context
-
-
-
-
-
-# import os
 # from django.conf import settings
 # from django.shortcuts import render, get_object_or_404
-# from django.shortcuts import render
 # from django.contrib.auth.decorators import login_required
 # from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+#
 # from django.core.cache import cache
+#
+# import os
 # import json
 #
-# from django.views.decorators.cache import cache_page
+# from django.views.decorators.cache import cache_page, never_cache
 # from django.views.generic import DetailView
+#
 # from products.models import Product, ProductsCategory
 #
-# from django.views.generic.list import ListView
-#
 # MODULE_DIR = os.path.dirname(__file__)
+#
+#
+# # Create your views here.
+# # Контролер - функция
 #
 # def get_links_category():
 #     if settings.LOW_CACHE:
@@ -161,39 +57,28 @@ class ProductDetail(DetailView):
 #     else:
 #         return Product.objects.filter(is_active=True).select_related()
 #
+#
 # def index(request):
 #     context = {'title': 'GeekShop'}
 #     return render(request, 'products/index.html', context)
+# @cache_page(3600)
+# def products(request, id=None, page=1):
+#     products = Product.objects.filter(category_id=id).select_related(
+#         'category') if id != None else Product.objects.all().select_related('category')
+#     # products = get_links_product()
+#     paginator = Paginator(products, per_page=3)
+#     try:
+#         products_paginator = paginator.page(page)
+#     except PageNotAnInteger:
+#         products_paginator = paginator.page(1)
+#     except EmptyPage:
+#         products_paginator = paginator.page(paginator.num_pages)
 #
-#
-# # def products(request,id=None,page=1):
-# #
-# #     products = Product.objects.filter(category_id = id).select-related('category') if id != None else Product.objects.all().select-related('category')
-# #     paginator = Paginator(products,per_page=3)
-# #     try:
-# #         products_paginator = paginator.page(page)
-# #     except PageNotAnInteger:
-# #         products_paginator = paginator.page(1)
-# #     except EmptyPage:
-# #         products_paginator = paginator.page(paginator.num_pages)
-# #
-# #     context = {'title': 'Каталог',
-# #                'category':  ProductsCategory.objects.all(),
-# #                }
-# #     context['products'] = products_paginator
-# #     return render(request, 'products/products.html', context)
-#
-#
-# # class ProductListView(ListView):
-# #     model = Product
-# #     template_name = 'products/products.html'
-# #     paginate_by = 3
-# #
-# #
-# #     def get_context_data(self, *args, **kwargs):
-# #         context = super(ProductListView, self).get_context_data(**kwargs)
-# #         context['title'] = 'Каталог'
-# #         return context
+#     context = {'title': 'Каталог',
+#                'category': ProductsCategory.objects.filter(is_active=True),
+#                }
+#     context['products'] = products_paginator
+#     return render(request, 'products/products.html', context)
 #
 #
 # class ProductDetail(DetailView):
@@ -204,9 +89,119 @@ class ProductDetail(DetailView):
 #     template_name = 'products/product_detail.html'
 #     context_object_name = 'product'
 #
+#
 #     def get_context_data(self, category_id=None, *args, **kwargs):
 #         """Добавляем список категорий для вывода сайдбара с категориями на странице каталога"""
 #         context = super().get_context_data()
+#
 #         context['product'] = get_product(self.kwargs.get('pk'))
 #         context['categories'] = ProductsCategory.objects.all()
 #         return context
+
+
+
+
+
+import os
+from django.conf import settings
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.cache import cache
+import json
+
+from django.views.decorators.cache import cache_page
+from django.views.generic import DetailView
+from products.models import Product, ProductsCategory
+
+from django.views.generic.list import ListView
+
+MODULE_DIR = os.path.dirname(__file__)
+
+def get_links_category():
+    if settings.LOW_CACHE:
+        key = 'links_category'
+        links_category = cache.get(key)
+
+        if links_category is None:
+            links_category = ProductsCategory.objects.filter(is_active=True)
+            cache.set(key, links_category)
+        return links_category
+    else:
+        return ProductsCategory.objects.filter(is_active=True)
+
+def get_product(pk):
+    if settings.LOW_CACHE:
+        key = f'product{pk}'
+        product = cache.get(key)
+
+        if product is None:
+            product = get_object_or_404(Product,pk=pk)
+            cache.set(key, product)
+        return product
+    else:
+        return get_object_or_404(Product,pk=pk)
+
+
+def get_links_product():
+    if settings.LOW_CACHE:
+        key = 'links_product'
+        links_product = cache.get(key)
+
+        if links_product is None:
+            links_product = Product.objects.filter(is_active=True).select_related()
+            cache.set(key, links_product)
+        return links_product
+    else:
+        return Product.objects.filter(is_active=True).select_related()
+
+def index(request):
+    context = {'title': 'GeekShop'}
+    return render(request, 'products/index.html', context)
+
+
+# def products(request,id=None,page=1):
+#
+#     products = Product.objects.filter(category_id = id).select-related('category') if id != None else Product.objects.all().select-related('category')
+#     paginator = Paginator(products,per_page=3)
+#     try:
+#         products_paginator = paginator.page(page)
+#     except PageNotAnInteger:
+#         products_paginator = paginator.page(1)
+#     except EmptyPage:
+#         products_paginator = paginator.page(paginator.num_pages)
+#
+#     context = {'title': 'Каталог',
+#                'category':  ProductsCategory.objects.all(),
+#                }
+#     context['products'] = products_paginator
+#     return render(request, 'products/products.html', context)
+
+
+# class ProductListView(ListView):
+#     model = Product
+#     template_name = 'products/products.html'
+#     paginate_by = 3
+#
+#
+#     def get_context_data(self, *args, **kwargs):
+#         context = super(ProductListView, self).get_context_data(**kwargs)
+#         context['title'] = 'Каталог'
+#         return context
+
+
+class ProductDetail(DetailView):
+    """
+    Контроллер вывода информации о продукте
+    """
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, category_id=None, *args, **kwargs):
+        """Добавляем список категорий для вывода сайдбара с категориями на странице каталога"""
+        context = super().get_context_data()
+        context['product'] = get_product(self.kwargs.get('pk'))
+        context['categories'] = ProductsCategory.objects.all()
+        return context
