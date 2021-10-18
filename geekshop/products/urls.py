3e89d99c9e2e3065fd8869eeabfdb3654199d4b3
eles <ivaneleskin2019@gmail.com>
2021-10-18 14:51:30 +0300

    add cashe

--- geekshop/products/urls.py
+++ geekshop/products/urls.py
@@ -1,30 +1,46 @@
-"""geekshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 app_name = 'products'
 
-from products.views import ProductListView, ProductDetail #products
+from products.views import products,ProductDetail
 
 urlpatterns = [
-    # path('', products, name='index'),
-    path('', ProductListView.as_view(), name='index'),
-    path('category/<int:pk>', ProductListView, name='category'),
-    path('page/<int:page>', ProductListView, name='page'),
+    # path('', cache_page(3600)(products), name='index'),
+    path('', products, name='index'),
+    path('category/<int:id>', products, name='category'),
+    path('page/<int:page>', products, name='page'),
     path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from django.contrib import admin
+# from django.urls import path
+#
+# app_name = 'products'
+#
+# from products.views import ProductListView, ProductDetail #products
+#
+# urlpatterns = [
+#     # path('', products, name='index'),
+#     path('', ProductListView.as_view(), name='index'),
+#     path('category/<int:pk>', ProductListView, name='category'),
+#     path('page/<int:page>', ProductListView, name='page'),
+#     path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
+# ]
 
